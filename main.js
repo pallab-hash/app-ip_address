44663a141e29a5d771a110434bcd2fb4f8f526c4
pallab-hash <pallab.acharyya@centurylink.com>
2020-03-09 11:57:32 +0000

    Lab 2 part 1 of 2

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 /*
+  Import the built-in path module.
+  See https://nodejs.org/api/path.html
+  The path module provides utilities for working with file and directory paths.
+  IAP requires the path module to access local file modules.
+  The path module exports an object.
+  Assign the imported object to variable path.
+*/
+const path = require('path');
+
+/**
+ * Import helper function module located in the same directory
+ * as this module. IAP requires the path object's join method
+ * to unequivocally locate the file module.
+ */
+const { getIpv4MappedIpv6Address } = require(path.join(__dirname, 'ipv6.js'));
+/*
   Import the ip-cidr npm package.
   See https://www.npmjs.com/package/ip-cidr
   The ip-cidr package exports a class.
@@ -28,6 +44,17 @@ function getFirstIpAddress(cidrStr, callback) {
     from: 1,
     limit: 1
   };
+  /**
+ * An object which will represent two strings ipv4 and ipv6
+ * 
+ * @param {ipv4} ipv4 address
+ * @param {ipv6} ipv6 address
+ * 
+ */
+const netFormats = {
+  ipv4: null,
+  ipv6: null,
+  };
 
   // Use the object's isValid() method to verify the passed CIDR.
   if (!cidr.isValid()) {
@@ -38,62 +65,19 @@ function getFirstIpAddress(cidrStr, callback) {
     // Notice the destructering assignment syntax to get the value of the first array's element.
     [firstIpAddress] = cidr.toArray(options);
   }
-  // Call the passed callback function.
-  // Node.js convention is to pass error data as the first argument to a callback.
+  
+/**
+ * It will create an object for net formats which will include ipv4 and ipv6 as a string attribute
+ **/
+const format = Object.create(netFormats);
+format.ipv4 = firstIpAddress;
+(firstIpAddress == null) ? format.ipv6 = null: format.ipv6 = getIpv4MappedIpv6Address(firstIpAddress);
+// Node.js convention is to pass error data as the first argument to a callback.
   // The IAP convention is to pass returned data as the first argument and error
   // data as the second argument to the callback function.
-  return callback(firstIpAddress, callbackError);
+  // Call the passed callback function.
+  return callback(format, callbackError);
 }
-
-/**
- * Calculates an IPv4-mapped IPv6 address.
- * @param {string} ipv4 - An IPv4 address in dotted-quad format.
- * @return {*} (ipv6Address) - An IPv6 address string or null if a run-time problem was detected.
- */
-function getIpv4MappedIpv6Address(ipv4) {
-
-  // Initialize return argument
-  let ipv6Address = null;
-
-  // Prepare to derive a Hex version of the dotted-quad decimal IPv4 address.
-  // Split the IPv4 address into its four parts.
-  let ipv4Quads = ipv4.split('.');
-  // Count the number of parts found.
-  let numIpv4Segments = ipv4Quads.length;
-
-  // Verify IPv4 had four parts.
-  if (numIpv4Segments === 4) {
-    let validQuads = true;
-    // Iterate over the IPv4 address parts and verify each segment was a number between 0 and 255.
-    for(let i=0; i < numIpv4Segments; i++) {
-      if( isNaN(Number(ipv4Quads[i])) || ipv4Quads[i] < 0 || ipv4Quads[i] > 255 ) {
-        validQuads = false;
-      }
-    }
-    // Passed IPv4 is valid. Now to derive an IPv4-mapped IPv6 address.
-    if (validQuads) {
-      // Hardcode the prefix. During refactor, we might want to make the prefix a const.
-      ipv6Address = "0:0:0:0:0:ffff:";
-      // Iterate over the IPv4 parts
-      for(let i=0; i < numIpv4Segments; i++) {
-        // Convert part to an integer, then convert to a hex string using method toString()
-        // with a base 16 (hex) encoding.
-        let hexString = parseInt(ipv4Quads[i]).toString(16);
-        // If hex is odd (single digit), prepend a '0'. This is why we wanted to work with a string.
-        if (hexString.length % 2)
-          hexString = '0' + hexString;
-        // Append hex part to evolving variable ipv6Address.
-        ipv6Address = ipv6Address + hexString;
-        // Add a colon to split the encoded address and match the IPv6 format.
-        if(i===1) {
-          ipv6Address = ipv6Address + ':';
-        }
-      }
-    }
-  }
-  return ipv6Address;
-}
-
 
 /*
   This section is used to test function and log any errors.
@@ -118,8 +102,8 @@ function main() {
       if (error) {
         console.error(`  Error returned from GET request: ${error}`);
       }
-      console.log(`  Response returned from GET request: ${data}`);
-    });
+        console.log(`  Response returned from GET request: ${`{"ipv4":"${data.ipv4}","ipv6":"${data.ipv6}"}`} `);
+       });
   }
   // Iterate over sampleIpv4s and pass the element's value to getIpv4MappedIpv6Address().
   for (let i = 0; i < sampleIpv4sLen; i++) {
@@ -133,6 +117,7 @@ function main() {
     }
   }
 }
+
 /*
   Call main to run it.
 */
